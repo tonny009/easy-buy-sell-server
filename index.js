1680a7fdcf477f668e494c84a8011d6937239001
tonny009 <tabassum.islam009@gmail.com>
2022-11-24 01:13:15 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors()); 0
+app.use(express.json());
+
+
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+app.get('/', async (req, res) => {
+    res.send('Easy Buy and sell server is running');
+})
+
+app.listen(port, () => console.log(`Easy Buy and sell  running on ${port}`))
